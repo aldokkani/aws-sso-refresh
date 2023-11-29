fd18f15f2c80c6af0e63b1c6587d5de2d4918728
aldokkani <hossam.aldokkani@gmail.com>
2023-11-29 13:09:40 +0200

    style: add eslint

--- index.js
+++ index.js
@@ -3,39 +3,11 @@ import { fromSSO } from '@aws-sdk/credential-provider-sso';
 import { writeFileSync } from 'fs';
 import { spawn } from 'child_process';
 import { CronJob } from 'cron';
-import { program } from 'commander';
+import options from './options.js';
 
-program
-  .option(
-    '--refresh-every <refresh-every>',
-    'Refresh every [frequency][unit] e.g. 5s, 1 minute, 2h',
-    '55m'
-  )
-  .option('--profile-sso <profile-sso>', 'AWS SSO profile name', 'default')
-  .option('--profile-credentials <profile-credentials>', 'AWS credentials file profile name', 'default')
-  .option('--headless', 'Start in headless mode', false)
-  .option(
-    '-t, --timeout <timeout>',
-    'Timeout in minutes for puppeteer browser to wait',
-    3
-  )
-  .option(
-    '--user-data-dir <user-data-dir>',
-    'User data directory (browser profile) for puppeteer',
-    '/tmp/puppeteer'
-  )
-  .option(
-    '--aws-credentials-file <aws-credentials-file>',
-    'AWS credentials file path',
-    `${process.env.HOME}/.aws/credentials`
-  );
-
-program.parse();
-
-const { refreshEvery, profileSso, profileCredentials, userDataDir, awsCredentialsFile } =
-  program.opts();
-let { headless, timeout } = program.opts();
-timeout = timeout * 60000;
+const { refreshEvery, profileSso, profileCredentials, userDataDir, awsCredentialsFile } = options;
+let { headless, timeout } = options;
+timeout *= 60000;
 headless = headless ? 'new' : false;
 
 function getCronExpression(input) {
@@ -44,12 +16,10 @@ function getCronExpression(input) {
 
   if (matches?.length !== 3) throw new Error(`Invalid refreshEvery: ${input}`);
 
-  const frequency = parseInt(matches[1]);
-  if (isNaN(frequency)) throw new Error(`Invalid refreshEvery: ${input}`);
+  const frequency = parseInt(matches[1], 10);
+  if (Number.isNaN(frequency)) throw new Error(`Invalid refreshEvery: ${input}`);
 
   const unit = matches[2][0];
-  if (!['s', 'm', 'h'].includes(unit))
-    throw new Error(`Invalid refreshEvery: ${input}`);
 
   switch (unit) {
     case 's':
@@ -58,40 +28,21 @@ function getCronExpression(input) {
       return `0 */${frequency} * * * *`;
     case 'h':
       return `0 0 */${frequency} * * *`;
+    default:
+      throw new Error(`Invalid refreshEvery: ${input}`);
   }
 }
 
 const cronTime = getCronExpression(refreshEvery);
 
-let job;
-job = CronJob.from({
-  cronTime,
-  onTick: () => {
-    console.log(
-      `AWS credentials are being refreshed...${
-        job ? ` [Next refresh => ${job.nextDate().toHTTP()}]` : ''
-      }`
-    );
-    refreshAwsCredentials();
-  },
-  start: true,
-  runOnInit: true,
-});
-
 function refreshAwsCredentials() {
-  const child = spawn('aws', [
-    'sso',
-    'login',
-    '--no-browser',
-    '--profile',
-    profileSso,
-  ]);
+  const child = spawn('aws', ['sso', 'login', '--no-browser', '--profile', profileSso]);
 
   child.stdout.on('data', async (data) => {
     const output = data.toString().trim();
     const url = output.match(/(https?:\/\/[^\s]+)/g)[1];
     if (!url) {
-      console.log(output);
+      console.info(output);
       return;
     }
 
@@ -121,9 +72,6 @@ function refreshAwsCredentials() {
 
     await page.close();
 
-    // Sleep for 1 second for credential-provider-sso to retrieve the new credentials
-    await new Promise((resolve) => setTimeout(resolve, 1000));
-
     const { accessKeyId, secretAccessKey, sessionToken } = await fromSSO({
       profile: profileSso,
     })();
@@ -144,10 +92,28 @@ function refreshAwsCredentials() {
   });
 
   child.on('close', (code) => {
-    if (code === 0) {
-      console.log(`AWS credentials were refreshed successfully!`);
-    } else {
-      console.error('AWS credentials were NOT refreshed! Exit code:', code);
+    if (code !== 0) {
+      return console.error(
+        'AWS credentials were NOT refreshed because SSO login has failed! Exit code:',
+        code,
+      );
     }
+    return console.info('AWS credentials were refreshed successfully!');
   });
 }
+
+let job;
+// eslint-disable-next-line prefer-const
+job = CronJob.from({
+  cronTime,
+  onTick: () => {
+    console.info(
+      `AWS credentials are being refreshed...${
+        job ? ` [Next refresh => ${job.nextDate().toHTTP()}]` : ''
+      }`,
+    );
+    refreshAwsCredentials();
+  },
+  start: true,
+  runOnInit: true,
+});
---
+++ options.js
@@ -0,0 +1,34 @@
+import { program } from 'commander';
+
+program
+  .option(
+    '--refresh-every <refresh-every>',
+    'Refresh every [frequency][unit] e.g. 5s, 1 minute, 2h',
+    '55m',
+  )
+  .option('--profile-sso <profile-sso>', 'AWS SSO profile name', 'default')
+  .option(
+    '--profile-credentials <profile-credentials>',
+    'AWS credentials file profile name',
+    'default',
+  )
+  .option('--headless', 'Start in headless mode', false)
+  .option(
+    '-t, --timeout <timeout>',
+    'Timeout in minutes for puppeteer browser to wait',
+    3,
+  )
+  .option(
+    '--user-data-dir <user-data-dir>',
+    'User data directory (browser profile) for puppeteer',
+    '/tmp/puppeteer',
+  )
+  .option(
+    '--aws-credentials-file <aws-credentials-file>',
+    'AWS credentials file path',
+    `${process.env.HOME}/.aws/credentials`,
+  );
+
+program.parse();
+
+export default program.opts();
