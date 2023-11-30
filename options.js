da1e75afb6fbdeff1158d112edfab9ca985b3051
aldokkani <hossam.aldokkani@gmail.com>
2023-11-30 13:21:11 +0200

    chore: update default values

--- options.js
+++ options.js
@@ -4,7 +4,7 @@ program
   .option(
     '--refresh-every <refresh-every>',
     'Refresh every [frequency][unit] e.g. 5s, 1 minute, 2h',
-    '55m',
+    '20m',
   )
   .option('--profile-sso <profile-sso>', 'AWS SSO profile name', 'default')
   .option(
@@ -13,11 +13,7 @@ program
     'default',
   )
   .option('--headless', 'Start in headless mode', false)
-  .option(
-    '-t, --timeout <timeout>',
-    'Timeout in minutes for puppeteer browser to wait',
-    3,
-  )
+  .option('-t, --timeout <timeout>', 'Timeout in minutes for puppeteer browser to wait', 5)
   .option(
     '--user-data-dir <user-data-dir>',
     'User data directory (browser profile) for puppeteer',
